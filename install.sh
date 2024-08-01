a1732c18c073884454065bc3fd5423a339d60b94
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-08-01 11:59:47 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/Documents/.dotfiles && cp -r . ~/Documents/.dotfiles
+cd ~/Documents/.dotfiles
+ln -s ~/Documents/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/Documents/.dotfiles/.gitconfig ~/.gitconfig
+
+git clone https://github.com/pwndbg/pwndbg.git .
+cd pwndbg
+./setup.sh
+
+sudo gpasswd -a $USER input
+sudo pacman -S xdotool libinput-gestures
+ln -s libinput-gestures.conf ~/.config/libinput-gestures.conf
+libinput-gestures-setup autostart start
