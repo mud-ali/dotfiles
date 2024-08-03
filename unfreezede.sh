7bdc5fd62b99eeb917b545093aec0922696ba970
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-08-03 16:10:09 -0400

    put into function

--- unfreezede.sh
+++ unfreezede.sh
@@ -1,7 +1,8 @@
-#!/bin/zsh
 
-session_id=$(loginctl list-sessions | grep tty2 | awk -F ' ' '{print $1}')
+function unfreezede {
+  session_id=$(loginctl list-sessions | grep tty2 | awk -F ' ' '{print $1}')
 
-loginctl unlock-session $session_id
+  loginctl unlock-session $session_id
 
-ps axf | grep kscreenlock | grep -v grep | awk '{print "kill -9 " $1}' | sudo sh
+  ps axf | grep kscreenlock | grep -v grep | awk '{print "kill -9 " $1}' | sudo sh
+}
