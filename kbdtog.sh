c20897c99cad77e27197340277cb0fbf90b9fd33
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-08-22 09:14:37 -0400

    cache keyboard id values to disk

--- kbdtog.sh
+++ kbdtog.sh
@@ -2,8 +2,12 @@ function kbdoff {
 	export KBDMASTER=$(xinput list | grep 'AT Translated' | sed "s/.*keyboard\\s\(\)//" | awk -F '[()]' '{print $2}')
 	export KBDSLAVE=$(xinput list | grep 'AT Translated' | sed "s/.*id=//" | sed "s/\\s.*//")
 	xinput float $KBDSLAVE
+	echo "$KBDMASTER" >/tmp/kbdmaster
+	echo "$KBDSLAVE" >/tmp/kbdslave
 }
 
 function kbdon {
+	export KBDMASTER=$(cat /tmp/kbdmaster)
+	export KBDSLAVE=$(cat /tmp/kbdslave)
 	xinput reattach $KBDSLAVE $KBDMASTER
 }
